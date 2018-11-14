5456b019398bd1d766c020a5ba5f00a9781abe8f
狐狸 <417045639@qq.com>
2018-11-14 21:14:33 +0800

    2018年11月14日 21:14:31

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,22 @@ const app = getApp()
 Page({
   data: {
     currentTab: 0,
-    colortop:'#f5c243'
+    colortop:'#f5c243',
+    goodslis:[
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'},
+      {title:'雅诗兰黛精华小棕瓶',price:'590.00',chun:'10'}
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -27,6 +42,28 @@ Page({
       title: '商城'
     });
   },
+  //
+  //获取当前滑块的index
+  bindchange:function(e){
+    const that  = this;
+    that.setData({
+      currentData: e.detail.current
+    })
+  },
+  //点击切换，滑块index赋值
+  checkCurrent:function(e){
+    const that = this;
+
+    if (that.data.currentData === e.target.dataset.current){
+        return false;
+    }else{
+
+      that.setData({
+        currentData: e.target.dataset.current
+      })
+    }
+  }
+
 
 
 
